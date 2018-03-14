7c9f026d8ecd8a2cd3706237f8d6b6887facfe42
An Nuyen <annuyen@gmail.com>
2018-03-14 03:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function getIssues() {
+const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then(data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      })
+    })
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = {title: issueTitle, body: issueBody}
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
