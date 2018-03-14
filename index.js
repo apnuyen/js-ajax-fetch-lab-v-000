25aba8c92f750866629d401357af5c612bb1c2d3
An Nuyen <annuyen@gmail.com>
2018-03-14 03:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `toke ${getToken()}`
+    }
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
 }
 
 function getToken() {
